61cd34e9dddeaf59f6365ba3da88483c0533f9d7
daichi kouno <15214kouno@yuai.ac.jp>
2021-10-09 12:07:34 +0900

    マイグレーションファイルの復旧

--- db/schema.rb
+++ db/schema.rb
@@ -29,4 +29,11 @@ ActiveRecord::Schema.define(version: 2021_09_27_114223) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
 end
